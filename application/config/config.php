57bac49a5346441638b0c0f0a4bde2fb3da229b2
Jacky Smith <JackySmithH2002@gmail.com>
2022-08-10 03:34:35 -0400

    update files

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://10.10.11.106/protoarch/';
+$config['base_url'] = 'https://cloud.azureavs.com/protoarch/';
 
 /*
 |--------------------------------------------------------------------------
