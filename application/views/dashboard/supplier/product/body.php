dfb7a19d485bb366994691d5e5937fdae2022fb1
Jacky Smith <JackySmithH2002@gmail.com>
2022-08-21 17:32:08 -0400

    update product body

--- application/views/dashboard/supplier/product/body.php
+++ application/views/dashboard/supplier/product/body.php
@@ -34,14 +34,16 @@
                     }
                 }
                 $subtotal=0; $vat_amount=0; $total_amount=0;
-                foreach ($lines as $key => $line) {
-                    $subtotal+=$line['amount_without_vat'];
-                    $vat_amount+=$line['amount_vat_value'];
-                    $total_amount+=$line['total_amount'];
+                if (count($lines)!=0) {
+                    foreach ($lines as $key => $line) {
+                        $subtotal+=$line['amount_without_vat'];
+                        $vat_amount+=$line['amount_vat_value'];
+                        $total_amount+=$line['total_amount'];
+                    }
+                    $total_subtotal+=$subtotal;
+                    $total_vat_amount+=$vat_amount;
+                    $total_total_amount+=$total_amount;
                 }
-                $total_subtotal+=$subtotal;
-                $total_vat_amount+=$vat_amount;
-                $total_total_amount+=$total_amount;
                 echo str_replace("_"," ", $result['name']);
                 echo $result['isremoved']?"(<span id='boot-icon' class='bi bi-circle-fill' style='font-size: 12px; color: rgb(255, 0, 0);''></span>)":"";
             ?>
