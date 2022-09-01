28cf668e6d03cc07a4a4f707a95223101fc205fe
Jacky Smith <JackySmithH2002@gmail.com>
2022-09-01 09:04:00 -0400

    update client invoice

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -422,6 +422,7 @@ class Home_model extends CI_Model {
         $list_lines=json_decode($lines, true);
         foreach ($list_lines as $index => $line) {
             if (str_contains($line['description'], '] - ')) {
+                return $line['description'];
                 $list = preg_split($pattern, $line['description'], -1, PREG_SPLIT_NO_EMPTY);
                 $code_ean = $list[0];
                 $productname = $list[1];
@@ -430,8 +431,6 @@ class Home_model extends CI_Model {
                 $query =    "SELECT *
                             FROM `product_lines`
                             WHERE `code_ean` = '$code_ean' AND `production_description` = '$productname' AND `quantity_on_document` >= '$qty'";
-
-                return $query;
 
                 $data = $this->db->query($query)->result_array();
 
