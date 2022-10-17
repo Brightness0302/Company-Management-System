a490956d0593df86691b0d98c339799b86125384
Jacky Smith <JackySmithH2002@gmail.com>
2022-10-17 02:21:06 -0400

    update cron job

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -397,11 +397,9 @@ class Home extends CI_Controller
             $db_names .= ' '.'database'.$i;
         }
 
-        // $output = shell_exec('crontab -l');
-        // file_put_contents('/tmp/crontab.txt', "5 * * * * mysqldump -u {$db_user} -p{$db_pwd} --opt --all-databases > {$bkp_file_path}$(date +'%d_%m_%Y_%H_%M_%S').sql".PHP_EOL);
-        // echo exec('crontab /tmp/crontab.txt');
-        shell_exec("crontab -l | { cat; echo '*/1    *    *    *    *    command'; } |crontab -");
-
+        $output = shell_exec('crontab -l');
+        file_put_contents('assets/tmp/crontab.txt', "5 * * * * mysqldump -u {$db_user} -p{$db_pwd} --opt --all-databases > {$bkp_file_path}$(date +'%d_%m_%Y_%H_%M_%S').sql".PHP_EOL);
+        echo exec('crontab assets/tmp/crontab.txt');
 
         // exec('crontab -r', $crontab_r);
         // exec('crontab -l', $crontab_l);
