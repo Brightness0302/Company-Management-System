bbe41403f8af24adaf5900ed90575ff09df77ea3
Jacky Smith <JackySmithH2002@gmail.com>
2022-10-17 13:05:32 -0400

    update cron job

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -398,8 +398,8 @@ class Home extends CI_Controller
         }
 
         $prev_crontab = shell_exec('crontab -l');
-        file_put_contents('assets/tmp/crontab.txt', "*/5 * * * * php /var/www/html/crm/index.php home backup\n".PHP_EOL);
-        $crontab_status = shell_exec('crontab /var/www/html/crm/assets/tmp/crontab.txt');
+        file_put_contents('assets/tmp/crontab.txt', "*/5 * * * * php /var/www/html/crm/index.php home backup".PHP_EOL);
+        $crontab_status = shell_exec('crontab assets/tmp/crontab.txt');
         echo $crontab_status;
 
         // exec('crontab -r', $crontab_r);
