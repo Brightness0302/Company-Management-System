ab01b86c57739bd6f152fc6dd02857a46c8746ea
Brightness0302 <Brightnes0302@proton.me>
2022-10-23 11:47:25 -0400

    update client invoice

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -115,6 +115,8 @@ class Home extends CI_Controller
             $data['stocks'][$index]['selling_amount_without_vat'] = $this->supplier->getdatafromstockid($companyid, $stock['id'], 'selling_amount_without_vat');
         }
 
+        $data['backups'] = $this->get_backups();
+
         $session['menu']="Dashboard";
         $session['submenu']="";
         $session['second-submenu']="NONE";
@@ -357,6 +359,7 @@ class Home extends CI_Controller
         $companyname = $this->session->userdata('companyname');
         $path = "assets/company/backups/".$companyname;
         $files = scandir($path);
+        return $files;
     }
     //backup function for mysql database
     public function backup_schedule() {
