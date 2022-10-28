09b98856a7d690430b36125bf923fbc4ec63c596
Brightness0302 <Brightnes0302@proton.me>
2022-10-28 08:47:25 -0400

    update dompdf

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -6,12 +6,13 @@ require_once(dirname(__FILE__) . '/dompdf/autoload.inc.php');
 class Pdf
 {
     function createPDF($html, $filename='', $download=TRUE, $paper='A4', $orientation='portrait'){
-        $dompdf = new Dompdf\Dompdf(['isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true]);
+        $dompdf = new Dompdf\Dompdf(['isRemoteEnabled' => true]);
         $dompdf->load_html($html);
         $dompdf->set_paper($paper, $orientation);
         $dompdf->render();
 
         $dompdf->stream($filename, array('Attachment' => 0));
+        exit();
         return true;
     }
 };
