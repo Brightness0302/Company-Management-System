69e2f7b754f2b808ae7617d9817ee1982ffe5758
Brightness0302 <Brightnes0302@proton.me>
2022-11-21 04:32:47 -0500

    update style for dashboard

--- application/views/dashboard/body.php
+++ application/views/dashboard/body.php
@@ -8,7 +8,6 @@
         <div class="d-flex align-items-center justify-content-between">
             <a href="index.html" class="logo d-flex align-items-center justify-center">
                 <img src="<?=base_url('assets/company/image/'.$company['id']).'.jpg'?>" alt="">
-                <span>asdf</span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
         </div><!-- End Logo -->
