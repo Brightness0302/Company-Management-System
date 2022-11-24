f78b87dff1aa2bca8f76cf670c1be1f60267d48e
Brightness0302 <Brightnes0302@proton.me>
2022-11-24 04:56:01 -0500

    update working details

--- application/views/dashboard/labor/workingdetails/body.php
+++ application/views/dashboard/labor/workingdetails/body.php
@@ -2,7 +2,7 @@
     <thead>
         <tr>
             <th class="w-48">Day of the month</th>
-            <th class="w-72">Select project</th>
+            <th class="w-64">Select project</th>
             <th>Work description</th>
         </tr>
     </thead>
