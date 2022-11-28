dd2f132e314562d3141c6284cc156a5495b3bf1f
Brightness0302 <Brightnes0302@proton.me>
2022-11-27 21:38:59 -0500

    coin details on table for supplier invoice

--- application/views/dashboard/body.php
+++ application/views/dashboard/body.php
@@ -5,7 +5,7 @@
     <header id="header" class="header fixed-top d-flex align-items-center">
 
         <div class="d-flex align-items-center justify-content-between">
-            <a href="index.html" class="logo d-flex align-items-center justify-center">
+            <a href="<?=base_url('home/dashboard')?>" class="logo d-flex align-items-center justify-center">
                 <img src="<?=base_url('assets/company/image/'.$company['id']).'.jpg'?>" alt="">
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
