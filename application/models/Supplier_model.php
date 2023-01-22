49f8c23300452a06195eea217e7e0d37505fd6de
brightness0302 <Brightnes0302@gmail.com>
2023-01-22 12:10:33 -0500

    show modal in recipe

--- application/models/Supplier_model.php
+++ application/models/Supplier_model.php
@@ -204,6 +204,8 @@ class Supplier_model extends CI_Model {
                     $query =    "SELECT *
                                 FROM `material_totalline`
                                 WHERE `stockid` = '$stockid' AND `expenseid` = '$expenseid' AND `serial_number` = '$serial_number' AND `units` = '$units' AND `vat` = '$vat' AND `makeup` = '$makeup' AND abs(`invoice_coin_rate` - '$invoice_coin_rate') <= 0.00001  AND `invoice_coin` = '$invoice_coin' AND abs(`main_coin_rate` - '$main_coin_rate') <= 0.00001 AND `main_coin` = '$main_coin' AND `acquisition_unit_price_on_invoice` = '$acquisition_unit_price_on_invoice'";
+                    echo $query;
+                    return;
 
                     $data = $this->db->query($query)->result_array();
 
@@ -514,7 +516,7 @@ class Supplier_model extends CI_Model {
 
         $this->db->where('id', $id);
         $res=$this->db->update('material', $data);
-        $this->savelines($companyid, $id, $main_coin, $invoice_coin, $invoice_coin_rate, $main_coin_rate, $lines);
+        return $this->savelines($companyid, $id, $main_coin, $invoice_coin, $invoice_coin_rate, $main_coin_rate, $lines);
         return $res;
     }
     //get date_of_reception, product_number, received_with_document for invoice
