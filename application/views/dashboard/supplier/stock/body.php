67c9c24ddf71c86b112deb9979d148090fdbfd22
brightness0302 <Brightnes0302@gmail.com>
2023-01-25 00:33:27 -0500

    change coininfo of stock management for chart

--- application/views/dashboard/supplier/stock/body.php
+++ application/views/dashboard/supplier/stock/body.php
@@ -3,6 +3,7 @@
     <canvas id="canvas" style="display: block; box-sizing: border-box; height: 560px; width: 1120px;" width="1120" height="560"></canvas>
 </div>
 <script>
+const coinInfo = "<?=(($company['Coin']=="EURO")?"€":(($company['Coin']=="POUND")?"£":(($company['Coin']=="USD")?"$":"LEI")))?>";
 var barChartData = {
     labels: [
         <?php foreach ($stocks as $stock):?>
@@ -52,11 +53,11 @@ window.onload = function() {
                     label: function(t, d) {
                        if (t.datasetIndex === 0) {
                           var xLabel = d.datasets[t.datasetIndex].label;
-                          var yLabel = t.yLabel + ' €';
+                          var yLabel = t.yLabel + ' ' + coinInfo;
                           return xLabel + ': ' + yLabel;
                        } else if (t.datasetIndex === 1) {
                           var xLabel = d.datasets[t.datasetIndex].label;
-                          var yLabel = t.yLabel >= 1000 ? t.yLabel.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " €" : t.yLabel + " €";
+                          var yLabel = t.yLabel >= 1000 ? t.yLabel.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " " + coinInfo : t.yLabel + " " + coinInfo;
                           return xLabel + ': ' + yLabel;
                        }
                     }
@@ -75,9 +76,9 @@ window.onload = function() {
                         beginAtZero: true,
                         callback: function(value, index, values) {
                             if (parseInt(value) >= 1000) {
-                                return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " €";
+                                return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " " + coinInfo;
                             } else {
-                                return parseInt(value*10)/10.0 + " €";
+                                return parseInt(value*10)/10.0 + " " + coinInfo;
                             }
                         }
                     }
